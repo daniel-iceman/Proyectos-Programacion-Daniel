28ddd9f8ab4a22d63a6411a343a5e338679a37bf
danielHome <md.danieldiaz@gmail.com>
2020-01-24 17:19:34 -0600

    Agregue 2 archivos Boton js y css

---
+++ fitness-app/src/components/Boton.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles/Welcome.css'
+
+class Boton extends React.Component{ 
+    render(){
+        return( 
+            <div>
+                <button  />
+            </div>
+        )
+    }
+}
